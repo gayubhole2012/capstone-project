28baa703cd5e77fa0295ea5cb73b42f951e00d3c
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-01-05 13:46:18 -0800

    added Main components

---
+++ little-lemon/src/components/Main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Main() {
+    return (
+ <>
+            <main>
+                <h1>Little Lemon</h1>
+                <article>
+                    <h3>Chicago</h3>
+                    <p>we are family owned</p>
+                    <p>Mediterranean restaurant</p>
+                    <p>focused on traditional</p>
+                    <p>recepies served with a modern</p>
+                    <p>twist</p>
+                </article>
+                <article>
+                    <section>
+                        <h3>Heading 1</h3>
+                        <p>
+                            Some text goes in here.
+                        </p>
+                    </section>
+                    <section>
+                        <h3>Heading 2</h3>
+                        <p>
+                            Some text goes in here.
+                        </p>
+                    </section>
+                    <section>
+                        <h3>Heading 3</h3>
+                        <p>
+                            Some text goes in here.
+                        </p>
+                    </section>
+                   </article>
+                    </main>
+                </>
+                )
+}
+                export default Main;
