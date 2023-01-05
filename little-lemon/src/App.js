db59e8a9674714e7aa674f3633ed11ee6381c4c1
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-01-05 11:33:38 -0800

    Base structure using semantic tag

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Little from './littlelemon.png'
 
 function App() {
   return (
+    <>
+    <header>
+      <img src={Little}/>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li><a href="order online.html">Order Online</a></li>
+        <li><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
+    <main>
+      <h1>Little Lemon</h1>
+      <article>
+        <h3>Chicago</h3>
+        <p>we are family owned</p>
+        <p>Mediterranean restaurant</p>
+        <p>focused on traditional</p>
+        <p>recepies served with a modern</p>
+        <p>twist</p>
+      </article>
+    </main>
+    <footer>
+      <p>Copyright Little Lemon</p>
+    </footer>
     <div className="App">
-     <h1>HomePage</h1>
+    
     </div>
+    </>
   );
 }
 
