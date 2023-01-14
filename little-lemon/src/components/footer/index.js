1562233221d41187f398b26533baaa35d5e73be0
Gayatri <gayatri@admins-MacBook-Pro.local>
2023-01-13 16:14:34 -0800

    added footer

--- little-lemon/src/components/footer/index.js
+++ little-lemon/src/components/footer/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './style.css';
 
-function Footer(){
-return (
- <>
-   <footer>
-      <p>Copyright Little Lemon</p>
-    </footer>
- </>
-)
+
+function Footer() {
+  return (
+    <>
+      <footer className='full-width'>
+        <div className='inner-width'>
+          <div className='footer-left'>
+            <h4>Social media link</h4>
+            <li className=''><a href=" ">Address</a></li>
+            <li className=''><a href=" ">Email</a></li>
+            <li className=''><a href=" ">Phone Number</a></li>
+            </div>
+            <div className='footer-right'>
+            <h4>Contact Us</h4>
+            <li className=''><a href=" ">Address</a></li>
+            <li className=''><a href=" ">Email</a></li>
+            <li className=''><a href=" ">Phone Number</a></li>
+            </div>
+        </div>
+      </footer>
+    </>
+  )
 }
 export default Footer;
